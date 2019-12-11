57f0d65ed4b1192101d6ff24499a6ff4ea26bb95
naliya <58774558+naliya@users.noreply.github.com>
2019-12-11 22:17:49 +0800

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Resume</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none"> </span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#objective">Objective</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#AcademicAchievement">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#WorkingExperiences">Experiences</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#FinalYearProject">Final Year Project</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#SkillsCompetencies">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#CoCurriculumActivities">Co-Curriculum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#SpecialActivities">Special Activity</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#references">References</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="objective">
+      <div class="w-100">
+        <h1 class="mb-0">Nurul Aliya
+          <span class="text-primary">Abd Hadi</span>
+        </h1>
+        <div class="subheading mb-5">Cheras, Kuala Lumpur · (+60) 14-716-9564 ·
+          <a href="mailto:name@email.com">nurulaliyahadii@gmail.com</a></div>
+        <p class="lead mb-5">
+        I would like to apply for a position that allows me to utilize my skills for personal growth and 
+        to render a good service and at the same time to broaden my knowledge and experience for further 
+        career improvement.
+        </p>
+        <p class="lead mb-5">
+        I describe myself as a perceptive and empathic personality person which are good 
+        at noticing or realizing things, especially things that are not obvious and understand the other 
+        person's perspective and reality. I am also friendly and easy to get along with people.
+        </p>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="AcademicAchievement">
+      <div class="w-100">
+        <h2 class="mb-5">Education</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">University of Technology Malaysia</h3>
+            <div class="subheading mb-3">Bachelor of Computer Science (Bioinformatics)</div>
+            <p>GPA: 3.70</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2015 - 2019</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Malacca Matriculation College</h3>
+            <div class="subheading mb-3">Module 1 - Science</div>
+            <p>GPA: 3.67</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2014 - 2015</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Science Banting School</h3>
+            <div class="subheading mb-3"> Science Stream</div>
+            <p>SPM : 7A 2B</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2011 - 2013</span>
+          </div>
+        </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="WorkingExperiences">
+      <div class="w-100">
+        <h2 class="mb-5">Experiences</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Data Science, TM Research & Development Sdn Bhd (3rd September 2018 – 18th January 2019)</h3>
+          <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Develop data collector from various API into database.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Develop API for backend system of the current project.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Write script for data analytic problem.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Involved in System Integration Testing (SIT) for each module.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Re-tested bugs to verify all database entries.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Tracked error and provide status report during scrum meeting.</li>  
+            </ul>
+        </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Data Entry Accounting Department, Hero Market (2016)</h3>
+            <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Enter customer orders, vendor receipts, and invoices into offline system.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Compile statistical reports on payments, orders, and outstanding invoices.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Resolved customer concerns efficiently and expeditiously.</li>
+            </ul>
+        </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Retail – Salesgirl, Watson Group (2015)</h3>
+          <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Created merchandise displays to highlight featured items according to corporate plans.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Helped maintained inventory and replenished merchandise.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Greeted customers and provided friendly help while building customer relationships.</li>
+          </ul>
+          </div>
+        </div>        
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Pharmacy Assistant, Sihat Pharmacy (2014)</h3>
+            <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Organized files and patients’ records.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Receive prescriptions and communicate required medications to pharmacist.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Monitor medication stock and communicate with pharmacist when supplies are low.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Assist pharmacist with drug preparation and stock inventory.</li>
+            </ul>
+        
+        </div>
+        </div>
+
+        <h3 class="mb-0">Tuition Teacher, Smart Reader Kids (2014)</h3>
+            <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Delivered interactive activities to an average 12 students between 5-7 years of age.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Implemented program that allowed students to act out characters.</li>
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Tidying up the classroom at the end of a lesson.</li>
+            </ul>
+        </div>
+        </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="FinalYearProject">
+      <div class="w-100">
+        <h2 class="mb-5">Final Year Project</h2>
+        <div class="subheading mb-3">Comparison of Support Vector Machine and Artificial Neural Network to Classify Dental Implant (Research Based)</div>
+          <p class="lead mb-5">
+          My final year project includes a programming in machine learning for two different algorithms 
+          which are Support Vector Machine and Artificial Neural Network. It can classify the dataset 
+          into the presence and absence of dental implant. When new data inserted, the program will 
+          determine the type of the data and classify it according to class. For this project, I am 
+          using Anaconda as my programming platform and python as my programming language. 
+          Preprocessing the data is really important to ensure the results obtain is correct.
+          </p>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="SkillsCompetencies">
+      <div class="w-100">
+        <h2 class="mb-5">Skills</h2>
+        <div class="container">
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Soft Skills</h3>
+          <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-square"></i>
+            Flexible</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+            Teamwork</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+            Fast Thinker</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+           Reliable</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+          Leadership</li>
+         </ul>
+          </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+          <h3 class="mb-0">Hard Skills</h3>
+          <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-square"></i>
+            Computer Programming (Python, .Net , C#, C++, Java)</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+            Web Development</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+           Database (SQL)</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+           Microsoft Office</li>
+          <li>
+            <i class="fa-li fa fa-square"></i>
+           Research</li>
+        </ul>
+           
+          </div>
+        </div>
+
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="CoCurriculumActivities">
+      <div class="w-100">
+        <h2 class="mb-5">Co-Curriculum</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Vice President</h3>
+            <div class="subheading mb-3">Technopreneurs Business Plan Competition (12th May 2018)</div>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Executive of Exhibition, Decoration & Booth Department</h3>
+            <div class="subheading mb-3"> UTM Career Carnival 2018 (17th – 18th April 2018)</div>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Google Students</h3>
+            <div class="subheading mb-3"> Google Games Kuala Lumpur ( 17th March 2018)</div>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Participant</h3>
+            <div class="subheading mb-3"> Malaysia Bioinformatics Conference (24th - 25th February 2018)</div>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Executive of Sports Department</h3>
+            <div class="subheading mb-3"> Kejohanan Sukan Kolej Mahasiswa 2017 (SUKAM’17) (7th – 14th November 2017)</div>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Participant</h3>
+            <div class="subheading mb-3"> UTM Cyber Forensics Challenge (18th September 2017)</div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="SpecialActivity">
+      <div class="w-100">
+        <h2 class="mb-5">Special Activity</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">UTM Global Outreach Program</h3>
+            <div class="subheading mb-3">Tokyo University of Agricultural and Technology</div>
+            <p>To gain new cultural and international experience</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">24th – 30th June 2018</span>
+          </div>
+        </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="references">
+      <div class="w-100">
+        <h2 class="mb-5">References</h2>
+        <div class="container">
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Dr. Sharin Hazlin binti Huspi</h3>
+            <h4 class="subheading mb-3">Academic Advisor,</h4>
+            <h4 class="subheading mb-3">School of Computing, Faculty of Engineering,</h4>
+            <h4 class="subheading mb-3">University of Technology Malaysia</h4>
+            <h4 class="subheading mb-3">sharin@utm.my</h4>
+            <h4 class="subheading mb-3">Tel: +60197710141</h4>
+           </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Dr Zuraini binti Ali Shah</h3>
+            <h4 class="subheading mb-3">Senior Lecturer,</h4>
+            <h4 class="subheading mb-3">School of Computing, Faculty of Engineering,
+            <h4 class="subheading mb-3">University of Technology Malaysia</h4>
+            <h4 class="subheading mb-3">aszuraini@utm.my</h4>
+            <h5 class="subheading mb-3">Tel: +6075538808</h5>
+           </div>
+        </div>
+        
+          </div>
+        </div>
+    </section>
+
+    <hr class="m-0">
+
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
+
+</body>
+
+</html>
